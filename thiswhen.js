6f7ef440c1046258ed997e93d1f93c0b0d41c243
rektide <rektide@voodoowarez.com>
2016-04-13 23:17:13 -0400

    Add a de-spreader. "rest"?

--- thiswhen.js
+++ thiswhen.js
@@ -1,4 +1,18 @@
-module.exports= function(fn){
+module.exports= function(fn, opts){
+	opts= opts|| {}
+	if(opts.rest){
+		return this.then(function(val){
+			var this_
+			if(!val.unshift){
+				this_= val
+				val= undefined
+			}else{
+				// for arraylike, take first arg, spread remaining out
+				this_= val.unshift()
+			}
+			return fn.apply(this_, val)
+		})
+	}
 	return this.then(function(val){
 		return fn.call(val)
 	})
